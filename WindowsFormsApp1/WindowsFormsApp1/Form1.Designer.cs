4da91dda9544a7ce6f2ad5dca883800ea7dd4b68
wonae789 <wonae789@naver.com>
2019-12-02 04:19:50 +0900

    로그인 화면

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ID_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PW_text = new System.Windows.Forms.TextBox();
+            this.Login_button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ID_text
+            // 
+            this.ID_text.Location = new System.Drawing.Point(173, 167);
+            this.ID_text.Name = "ID_text";
+            this.ID_text.Size = new System.Drawing.Size(100, 21);
+            this.ID_text.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PW";
+            // 
+            // PW_text
+            // 
+            this.PW_text.Location = new System.Drawing.Point(173, 204);
+            this.PW_text.Name = "PW_text";
+            this.PW_text.PasswordChar = '*';
+            this.PW_text.Size = new System.Drawing.Size(100, 21);
+            this.PW_text.TabIndex = 3;
+            // 
+            // Login_button
+            // 
+            this.Login_button.Location = new System.Drawing.Point(295, 167);
+            this.Login_button.Name = "Login_button";
+            this.Login_button.Size = new System.Drawing.Size(74, 58);
+            this.Login_button.TabIndex = 4;
+            this.Login_button.Text = "Login";
+            this.Login_button.UseVisualStyleBackColor = true;
+            this.Login_button.Click += new System.EventHandler(this.Login_button_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(165, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 48);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "H.soft";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 344);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Login_button);
+            this.Controls.Add(this.PW_text);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ID_text);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "로그인 화면";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ID_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PW_text;
+        private System.Windows.Forms.Button Login_button;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_button_Click(object sender, EventArgs e)
+        {
+
+            // con 객체 DB 연결.
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Jeahun\Desktop\Dev\Winform_1\WindowsFormsApp1\Data5.mdf;Integrated Security=True;Connect Timeout=30");
+
+            // sda 객체를 활용하여 원하는 자료를 뽑아낸다.
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) from USERINFO where USERNAME='" + ID_text.Text + "' and PASSWORD = '" + PW_text.Text + "'", con);
+
+            // 새로운 테이블 생성 , sda 데이터 담는 용도.
+            DataTable newTable = new DataTable();
+
+            sda.Fill(newTable);
+
+            if (newTable.Rows[0][0].ToString() == "1") // 데이터가 있을경우
+            {
+                //로그인 성공
+                //this.Hide;
+                Mainform mainform1 = new Mainform();
+                mainform1.Show();
+            }
+            else
+            {
+                //로그인 실패
+                MessageBox.Show("아이디 혹은 비밀번호가 일치하지 않습니다.");
+            }
+
+            //this.Hide();
+
+
+
+        }
+    }
+}
